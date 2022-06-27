b912f1c4afb3516cc8672dc89c2523318c4bef17
brian.jang <brian.jang@twolinecode.com>
2022-06-27 16:15:03 +0900

    feat(behavioral): add template method

---
+++ dp/src/behavioral/templateMethod/HouseTemplate.java
@@ -0,0 +1,26 @@
+package behavioral.templateMethod;
+
+public abstract class HouseTemplate {
+
+    // template method, final so subclasses can't override
+    public final void buildHouse() {
+        buildFoundation();
+        buildPillars();
+        buildWalls();
+        buildWindows();
+        System.out.println("House is built.");
+    }
+
+    private void buildWindows() {
+        System.out.println("Building Glass Windows");
+    }
+
+    private void buildFoundation() {
+        System.out.println("Building foundation with cement,iron rods and sand");
+    }
+
+    //methods to be implemented by subclasses
+    public abstract void buildWalls();
+    public abstract void buildPillars();
+
+}
